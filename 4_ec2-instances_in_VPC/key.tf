76f813031ecb911d67cf1e67d802ddf82020d26e
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-06-27 08:48:39 +0530

    Delete 4_ec2-instances_in_VPC directory

--- 4_ec2-instances_in_VPC/key.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_key_pair" "devopsmonks" {
-  key_name = "devopsmonks"
-
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-
-
-
-}
--- 4_ec2-instances_in_VPC/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip" {
-    description = "The public ip for SSH access"
-    value = aws_instance.example.public_ip
-}
